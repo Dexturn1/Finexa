dbf2627dc4e7d96a8f1a975f5e2772ef4c85259b
prabhat <kapkotiprabhat@gmail.com>
2026-08-03 00:03:23 +0530

    chore: comfigurer project setup and database connection

---
+++ src/main/java/com/finexa/finexa/FinexaApplication.java
@@ -0,0 +1,13 @@
+package com.finexa.finexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinexaApplication.class, args);
+	}
+
+}
