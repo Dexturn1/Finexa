9a81ef2f004925108381ae563be89b782788c5d0
prabhat <kapkotiprabhat@gmail.com>
2026-08-09 08:41:54 +0530

    feat(user): implement user controller endpoints

---
+++ src/main/java/com/finexa/finexa/auth_users/UserController.java
@@ -0,0 +1,52 @@
+package com.finexa.finexa.auth_users;
+
+
+
+import com.finexa.finexa.auth_users.dtos.UpdatePasswordRequest;
+import com.finexa.finexa.auth_users.dtos.UserDTO;
+import com.finexa.finexa.auth_users.services.UserService;
+import com.finexa.finexa.res.Response;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+    private final UserService userService;
+
+
+    @GetMapping
+    public ResponseEntity<Response<Page<UserDTO>>> getAllUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "50") int size
+    ){
+        return ResponseEntity.ok(userService.getAllUsers(page, size));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<Response<UserDTO>> getMyProfile(){
+        return ResponseEntity.ok(userService.getMyProfile());
+    }
+
+
+    @PutMapping("/update-password")
+    public ResponseEntity<Response<?>> updatePassword(@RequestBody @Valid UpdatePasswordRequest updatePasswordRequest){
+        return ResponseEntity.ok(userService.updatePassword(updatePasswordRequest));
+    }
+
+    @PutMapping("/profile-picture")
+    public ResponseEntity<Response<?>> uploadProfilePicture(@RequestParam("file")MultipartFile file){
+        return ResponseEntity.ok(userService.uploadProfilePicture(file));
+    }
+
+
+
+
+}
